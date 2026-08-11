572743eb1ef70e5de6efe3726895f0fce361c428
Matheus Goulart Leite da Fonseca <ra2457097>
2026-08-10 21:28:37 -0300

    inicio da refatoracao

--- src/java/com/template/controller/MainController.java
+++ src/java/com/template/controller/MainController.java
@@ -1,11 +1,11 @@
 package com.template.controller;
 
 import static com.template.validacao.FilmeValidador.*;
+import static com.template.util.FormUtil.*;
 import static com.template.util.DialogUtil.*;
 import com.template.model.dao.CinemaDAO;
 import com.template.model.dto.CinemaDTO;
 import javafx.scene.control.Label;
-import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -28,7 +28,7 @@ public class MainController {
     @FXML
     private Button btnAlterar;
     @FXML
-    private Button btnExcluir;
+    private Button btnDeletar;
     @FXML
     private TextField txtNome;
     @FXML
@@ -71,9 +71,6 @@ public class MainController {
         colNotaIMDB.setCellValueFactory(new PropertyValueFactory<>("notaIMDB"));
 
         carregarFilmes();
-
-
-
     }
 
     @FXML
@@ -102,97 +99,86 @@ public class MainController {
             carregarFilmes();
             btnLimparAction();
 
-            lblMensagem.setText("Filme salvo com sucesso!");
-            lblMensagem.setStyle("-fx-text-fill: green;");
+            //lblMensagem.setText("Filme salvo com sucesso!");
+            //lblMensagem.setStyle("-fx-text-fill: green;");
             showInfo("Filme salvo com sucesso!");
 
         } catch (Exception e) {
-            lblMensagem.setText("Erro ao salvar o filme.");
-            lblMensagem.setStyle("-fx-text-fill: red;");
+            //lblMensagem.setText("Erro ao salvar o filme.");
+            //lblMensagem.setStyle("-fx-text-fill: red;");
             showError("Erro ao salvar o filme");
             }
         }
     }
-
     @FXML
     private void btnLimparAction() {
-
-        if(validarLimpar(txtNome.getText(), txtGenero.getText(), txtAnoLancamento.getText(), txtBilheteria.getText(), txtNotaIMDB.getText())) {
-
-            txtNome.clear();
-            txtGenero.clear();
-            txtAnoLancamento.clear();
-            txtBilheteria.clear();
-            txtNotaIMDB.clear();
-
-            // Limpa a mensagem da tela quando o usuário limpa os campos
-            lblMensagem.setText("");
-        }
+        limparCampo(txtNome,txtGenero,txtAnoLancamento,txtBilheteria,txtNotaIMDB);
     }
 
     @FXML
     private void btnAlterarAction(ActionEvent event) {
 
-        if(validarAlterar(txtNome.getText(), txtGenero.getText(), txtAnoLancamento.getText(), txtBilheteria.getText(), txtNotaIMDB.getText())) {
+        if (validarAlterar(txtNome.getText(), txtGenero.getText(), txtAnoLancamento.getText(), txtBilheteria.getText(), txtNotaIMDB.getText())) {
 
-        CinemaDAO cinemaDAO = new CinemaDAO();
+            CinemaDAO cinemaDAO = new CinemaDAO();
 
-        try {
+            try {
+                CinemaDTO filmeSelecionado = tblCinema.getSelectionModel().getSelectedItem();
+
+                if (filmeSelecionado == null) {
+                    //lblMensagem.setText("Selecione um filme na tabela para alterar.");
+                    //lblMensagem.setStyle("-fx-text-fill: orange;");
+                    showInfo("Selecione um filme na tabela para alterar.");
+                    return;
+                }
+
+                CinemaDTO objCinemaDTO = new CinemaDTO();
+                objCinemaDTO.setId(filmeSelecionado.getId());
+                objCinemaDTO.setNome(txtNome.getText());
+                objCinemaDTO.setGenero(txtGenero.getText());
+                objCinemaDTO.setAnoLancamento(Integer.parseInt(txtAnoLancamento.getText()));
+                objCinemaDTO.setBilheteria(Double.parseDouble(txtBilheteria.getText().replace(",", ".")));
+                objCinemaDTO.setNotaIMDB(Double.parseDouble(txtNotaIMDB.getText().replace(",", ".")));
+
+                cinemaDAO.alterarFilme(objCinemaDTO);
+
+                carregarFilmes();
+                btnLimparAction();
+
+                //lblMensagem.setText("Filme alterado com sucesso!");
+                //lblMensagem.setStyle("-fx-text-fill: green;");
+                showInfo("Filme alterado com sucesso");
+            } catch (Exception e) {
+                //lblMensagem.setText("Erro ao alterar o filme.");
+                //lblMensagem.setStyle("-fx-text-fill: red;");
+                showError("Erro ao alterar o filme.");
+            }
+        }
+        }
+
+        @FXML
+        private void btnDeletarAction(ActionEvent event) {
+
+            CinemaDAO cinemaDAO = new CinemaDAO();
+
             CinemaDTO filmeSelecionado = tblCinema.getSelectionModel().getSelectedItem();
 
             if (filmeSelecionado == null) {
-                lblMensagem.setText("Selecione um filme na tabela para alterar.");
-                lblMensagem.setStyle("-fx-text-fill: orange;");
-                showInfo("Selecione um filme na tabela para alterar.");
+                //lblMensagem.setText("Selecione um filme na tabela para excluir.");
+                //lblMensagem.setStyle("-fx-text-fill: orange;");
+                showInfo("Selecione um filme na tabela para excluir.");
                 return;
             }
 
-            CinemaDTO objCinemaDTO = new CinemaDTO();
-            objCinemaDTO.setId(filmeSelecionado.getId());
-            objCinemaDTO.setNome(txtNome.getText());
-            objCinemaDTO.setGenero(txtGenero.getText());
-            objCinemaDTO.setAnoLancamento(Integer.parseInt(txtAnoLancamento.getText()));
-            objCinemaDTO.setBilheteria(Double.parseDouble(txtBilheteria.getText().replace(",", ".")));
-            objCinemaDTO.setNotaIMDB(Double.parseDouble(txtNotaIMDB.getText().replace(",", ".")));
-
-            cinemaDAO.alterarFilme(objCinemaDTO);
+            cinemaDAO.deletarFilme(filmeSelecionado.getId());
 
             carregarFilmes();
             btnLimparAction();
 
-            lblMensagem.setText("Filme alterado com sucesso!");
-            lblMensagem.setStyle("-fx-text-fill: green;");
-            showInfo("Filme alterado com sucesso");
-        } catch (Exception e) {
-            lblMensagem.setText("Erro ao alterar o filme.");
-            lblMensagem.setStyle("-fx-text-fill: red;");
-            showError("Erro ao alterar o filme.");
+            //lblMensagem.setText("Filme excluído com sucesso!");
+            //lblMensagem.setStyle("-fx-text-fill: red;");
+            showError("Filme excluido com sucesso");
         }
-    }
-
-    @FXML
-    private void btnDeletarAction(ActionEvent event) {
-
-        CinemaDAO cinemaDAO = new CinemaDAO();
-
-        CinemaDTO filmeSelecionado = tblCinema.getSelectionModel().getSelectedItem();
-
-        if (filmeSelecionado == null) {
-            lblMensagem.setText("Selecione um filme na tabela para excluir.");
-            lblMensagem.setStyle("-fx-text-fill: orange;");
-            showInfo("Selecione um filme na tabela para excluir.");
-            return;
-        }
-
-        cinemaDAO.deletarFilme(filmeSelecionado.getId());
-
-        carregarFilmes();
-        btnLimparAction();
-
-        lblMensagem.setText("Filme excluído com sucesso!");
-        lblMensagem.setStyle("-fx-text-fill: red;");
-        showError("Filme excluido com sucesso");
-    }
 
     @FXML
     private void carregarCampos() {
@@ -203,5 +189,8 @@ public class MainController {
         txtAnoLancamento.setText(String.valueOf(objCinemaDTO.getAnoLancamento()));
         txtBilheteria.setText(String.valueOf(objCinemaDTO.getBilheteria()));
         txtNotaIMDB.setText(String.valueOf(objCinemaDTO.getNotaIMDB()));
+
     }
-}
+        }
+
+
---
+++ src/java/com/template/util/FormUtil.java
@@ -0,0 +1,27 @@
+package com.template.util;
+
+import javafx.scene.control.TextField;
+
+public class FormUtil {
+
+    public static void limparCampo(
+            TextField nome,
+            TextField genero,
+            TextField anoLancamento,
+            TextField bilheteria,
+            TextField notaIMDB) {
+
+        nome.clear();
+        genero.clear();
+        anoLancamento.clear();
+        bilheteria.clear();
+        notaIMDB.clear();
+    }
+
+    public static void carregarFilmes(){
+
+    }
+
+
+    }
+
